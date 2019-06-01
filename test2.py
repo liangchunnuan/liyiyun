4135f48e4361b38ae78b579bb0b14c8f298d91f4
liangchunnuan <51155115+liangchunnuan@users.noreply.github.com>
2019-06-01 23:14:49 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,4 @@
-import network
-sta_if=network.WLAN(network.STA_IF)
-ap_if=network.WLAN(network.AP_IF)
-ap_if.active(False)
-if not sta_if.isconnected():
-    sta_if.active(True)
-    sta_if.connect('红米手机','123212321')
-    while not sta_if.isconnected():
-        psaa
-    print('network config:',sta_if.ifconfig())
+two
     
 
 
